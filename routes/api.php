6c440e65b83e03277cf2d0e8b59bf73f51623496
Genta-arya <mgentaarya@gmail.com>
2025-03-14 05:39:42 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return redirect()->away('https://www.google.com');
 });
+
 
 Route::middleware([DomainRestrictionMiddleware::class])->group(function () {
 
