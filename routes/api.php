9e2d1fc4ae12442850cc8e8f14525955c6116256
Genta-arya <mgentaarya@gmail.com>
2025-03-15 04:16:57 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
 use App\Http\Middleware\DomainRestrictionMiddleware;
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return redirect()->away('https://www.google.com');
